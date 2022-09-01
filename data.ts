205cd8fd3a65ee7ec1b8e9ba63356be840ee5461
Demir Boraj <demirboraj97@gmail.com>
2022-09-01 18:00:00 +0200

    1

---
+++ data.ts
@@ -0,0 +1,44 @@
+export const quotes = [
+    {
+      id: 1,
+      author: {
+        name: "John Lennon",
+        quote: "Life is what happens when you're busy making other plans.",
+        age: 40,
+        image:
+          "https://imgs.search.brave.com/gZtzxR-sGjRlkJd30fqQG3JPTz4NRjXwPUu_EbS8CqY/rs:fit:1008:700:1/g:ce/aHR0cHM6Ly90b2Rh/eWJpcnRoZGF5Lmlu/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE5/LzA2L0pvaG4tTGVu/bm9uLTdpbmZpLmpw/Zw",
+      },
+    },
+    {
+      id: 2,
+      author: {
+        name: "Walt Disney",
+        quote: "The way to get started is to quit talking and begin doing.",
+        age: 65,
+        image:
+          "https://imgs.search.brave.com/KdCrxLgB6M1tX6wzVpxYfoI1hCRZnGv5OEFgEcsJVbo/rs:fit:1200:1178:1/g:ce/aHR0cHM6Ly93d3cu/dGhlYmFsYW5jZXNt/Yi5jb20vdGhtYi9C/MXFLY2RBVVhqbTk1/MmwzYlFKMldVYVpr/NGs9LzE1MDB4MTE3/OC9maWx0ZXJzOmZp/bGwoYXV0bywxKS9H/ZXR0eUltYWdlcy02/MTM0OTA5NjgtNWMx/ZmU4YTA0NmUwZmIw/MDAxZTFkNmM2Lmpw/Zw",
+      },
+    },
+    {
+      id: 3,
+      author: {
+        name: "Mother Teresa",
+        quote:
+          "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+        age: 87,
+        image:
+          "https://imgs.search.brave.com/VTyWtZGYeTuw18rDf_qgIhcyHg3f5OsyCJMA07fth0g/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3Lmhh/cnZiaXNob3AuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA5L01vdGhlci1U/ZXJlc2EtQmFjYS1s/YXJnZXItc2l6ZS5q/cGc",
+      },
+    },
+    {
+      id: 4,
+      author: {
+        name: "Eleanor Roosevelt",
+        quote:
+          "The future belongs to those who believe in the beauty of their dreams.",
+        age: 78,
+        image:
+          "https://imgs.search.brave.com/BHOaX1gaVlEWnev7GNBGdoGL2qC5quGoWhNRc9coAQw/rs:fit:1200:1200:1/g:ce/aHR0cDovL2J0aW1l/c2hlcmFsZC5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTcv/MDUvUm9vc2V2ZWx0/LTEuanBn",
+      },
+    },
+  ];
