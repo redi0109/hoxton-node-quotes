36f024e936a715fb4b72758e22310c323c5094d4
Demir Boraj <demirboraj97@gmail.com>
2022-09-07 13:17:50 +0200

    creating author stuff

--- db/setup.ts
+++ db/setup.ts
@@ -6,48 +6,24 @@ const quotes = [
     {
       id: 1,
       quote: "Life is what happens when you're busy making other plans.",
-      author: {
-        name: "John Lennon",
-      
-        age: 40,
-        image:
-          "https://imgs.search.brave.com/gZtzxR-sGjRlkJd30fqQG3JPTz4NRjXwPUu_EbS8CqY/rs:fit:1008:700:1/g:ce/aHR0cHM6Ly90b2Rh/eWJpcnRoZGF5Lmlu/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE5/LzA2L0pvaG4tTGVu/bm9uLTdpbmZpLmpw/Zw",
-      },
+      authorId: 1
     },
     {
       id: 2,
       quote: "The way to get started is to quit talking and begin doing.",
-      author: {
-        name: "Walt Disney",
-       
-        age: 65,
-        image:
-          "https://imgs.search.brave.com/KdCrxLgB6M1tX6wzVpxYfoI1hCRZnGv5OEFgEcsJVbo/rs:fit:1200:1178:1/g:ce/aHR0cHM6Ly93d3cu/dGhlYmFsYW5jZXNt/Yi5jb20vdGhtYi9C/MXFLY2RBVVhqbTk1/MmwzYlFKMldVYVpr/NGs9LzE1MDB4MTE3/OC9maWx0ZXJzOmZp/bGwoYXV0bywxKS9H/ZXR0eUltYWdlcy02/MTM0OTA5NjgtNWMx/ZmU4YTA0NmUwZmIw/MDAxZTFkNmM2Lmpw/Zw",
-      },
+      authorId: 2 
     },
     {
       id: 3,
       quote:
       "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
-      author: {
-        name: "Mother Teresa",
-       
-        age: 87,
-        image:
-          "https://imgs.search.brave.com/VTyWtZGYeTuw18rDf_qgIhcyHg3f5OsyCJMA07fth0g/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3Lmhh/cnZiaXNob3AuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA5L01vdGhlci1U/ZXJlc2EtQmFjYS1s/YXJnZXItc2l6ZS5q/cGc",
-      },
+      authorId: 3 
     },
     {
       id: 4,
       quote:
       "The future belongs to those who believe in the beauty of their dreams.",
-      author: {
-        name: "Eleanor Roosevelt",
-      
-        age: 78,
-        image:
-          "https://imgs.search.brave.com/BHOaX1gaVlEWnev7GNBGdoGL2qC5quGoWhNRc9coAQw/rs:fit:1200:1200:1/g:ce/aHR0cDovL2J0aW1l/c2hlcmFsZC5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTcv/MDUvUm9vc2V2ZWx0/LTEuanBn",
-      },
+      authorId: 4 
     },
   ];
 
@@ -67,3 +43,42 @@ const quotes = [
   `)
 
   deleteAllQuotes.run();
+
+  const CreateQuote = db.prepare (`
+  INSERT INTO quotes (quote, authorId) VALUES (?, ?);
+  `);
+
+  for (let quote of quotes){
+    CreateQuote.run(quote.quote, quote.authorId)
+  }
+
+  const authors = [
+      {
+        name: "John Lennon",
+      
+        age: 40,
+        image:
+          "https://imgs.search.brave.com/gZtzxR-sGjRlkJd30fqQG3JPTz4NRjXwPUu_EbS8CqY/rs:fit:1008:700:1/g:ce/aHR0cHM6Ly90b2Rh/eWJpcnRoZGF5Lmlu/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE5/LzA2L0pvaG4tTGVu/bm9uLTdpbmZpLmpw/Zw",
+      },
+      {
+        name: "Walt Disney",
+       
+        age: 65,
+        image:
+          "https://imgs.search.brave.com/KdCrxLgB6M1tX6wzVpxYfoI1hCRZnGv5OEFgEcsJVbo/rs:fit:1200:1178:1/g:ce/aHR0cHM6Ly93d3cu/dGhlYmFsYW5jZXNt/Yi5jb20vdGhtYi9C/MXFLY2RBVVhqbTk1/MmwzYlFKMldVYVpr/NGs9LzE1MDB4MTE3/OC9maWx0ZXJzOmZp/bGwoYXV0bywxKS9H/ZXR0eUltYWdlcy02/MTM0OTA5NjgtNWMx/ZmU4YTA0NmUwZmIw/MDAxZTFkNmM2Lmpw/Zw",
+      },
+      {
+        name: "Mother Teresa",
+       
+        age: 87,
+        image:
+          "https://imgs.search.brave.com/VTyWtZGYeTuw18rDf_qgIhcyHg3f5OsyCJMA07fth0g/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3Lmhh/cnZiaXNob3AuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA5L01vdGhlci1U/ZXJlc2EtQmFjYS1s/YXJnZXItc2l6ZS5q/cGc",
+      },
+      {
+        name: "Eleanor Roosevelt",
+      
+        age: 78,
+        image:
+          "https://imgs.search.brave.com/BHOaX1gaVlEWnev7GNBGdoGL2qC5quGoWhNRc9coAQw/rs:fit:1200:1200:1/g:ce/aHR0cDovL2J0aW1l/c2hlcmFsZC5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTcv/MDUvUm9vc2V2ZWx0/LTEuanBn",
+      }
+  ]
